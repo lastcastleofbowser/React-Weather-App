d49dafcfa67e62e943e9078194e0d353f5e93c57
lastcastleofbowser <123087687+lastcastleofbowser@users.noreply.github.com>
2023-07-22 18:26:54 +0100

    more data being pulled for weather. foreacst data not set up yet. problems with wind speed and description

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,28 @@ interface WeatherData {
     humidity: number;
     pressure: number;
     windSpeed: number;
+    temp_min: number;
+    temp_max: number;
   };
 }
 
+interface ForecastData {
+  list: {
+    dt_txt: string;
+    main: {
+      temp: number;
+    };
+    weather: {
+      icon: string;
+      description: string;
+  }}};
+
+
 function App() {
   const API_KEY = process.env.REACT_APP_API_KEY; // Get API key from .env file
+
   const [location, setLocation] = useState("");
-  const [data, setData] = useState<WeatherData>({
+  const [weatherData, setWeatherData] = useState<WeatherData>({
     name: "",
     main: {
       description: "",
@@ -23,35 +38,63 @@ function App() {
       humidity: 0,
       pressure: 0,
       windSpeed: 0,
+      temp_min: 0,
+      temp_max: 0,
     },
   });
+const [forecastData, setForecastData] = useState<ForecastData>({
+  list: {
+    dt_txt: "",
+    main: {
+      temp: 0,
+    },
+    weather: {
+      icon: "",
+      description: "",
+    },
+}});
 
-  const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
+  const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`;
+  const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${API_KEY}&units=metric`;
 
   const fetchWeatherData = async () => {
     try {
       const response = await fetch(weatherURL);
       const data = await response.json();
-      setData(data);
-      console.log(data);
+      setWeatherData(data);
+      console.log("Weather Data:", data);
     } catch (error) {
       console.error("Error fetching weather data:", error);
     }
   };
 
-  useEffect(() => {
-    if (location && data.name === "") {
-      fetchWeatherData();
+  const fetchForecastData = async () => {
+    try {
+      const response = await fetch(forecastURL);
+      const data = await response.json();
+      setForecastData(data);
+      console.log("Forecast Data:", data);
+    } catch (error) {
+      console.error("Error fetching forecast data:", error);
     }
-  }, [location, data.name, weatherURL]);
+  };
+
+  useEffect(() => {
+    if (location && weatherData.name === "") {
+      fetchWeatherData();
+      fetchForecastData();
+    }
+  }, [location, weatherData.name, weatherURL]);
 
   const handleButtonClick = () => {
     fetchWeatherData();
+    fetchForecastData();
   };
 
-  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
       fetchWeatherData();
+      fetchForecastData();
     }
   };
 
@@ -63,14 +106,26 @@ function App() {
           placeholder="Search..."
           value={location}
           onChange={(e) => setLocation(e.target.value)}
-          onKeyPress={handleKeyPress} // Call handleKeyPress when Enter is pressed
+          onKeyDown={handleKeyDown}
         />
         <button onClick={handleButtonClick}>Search</button>
       </div>
       <div className="weather-container">
-        {data.name && <h2>{data.name}</h2>}
-        {data.main && <p>Temperature: {data.main.temp}</p>}
+        {weatherData.name && <h2>{weatherData.name}</h2>}
+        {weatherData.main && <p>Temperature: {weatherData.main.temp}</p>}
+        {weatherData.main && <p>Humidity: {weatherData.main.humidity}</p>}  
+        {weatherData.main && <p>Pressure: {weatherData.main.pressure}</p>}
+        {weatherData.main && <p>Wind Speed: {weatherData.main.windSpeed}</p>}
+        {weatherData.main && <p>Description: {weatherData.main.description}</p>}
+        {weatherData.main && <p>Min Temp: {weatherData.main.temp_min}</p>}
+        {weatherData.main && <p>Max Temp: {weatherData.main.temp_max}</p>}
       </div>
+      {/* <div className="forecast-container">
+        {forecastData.list && <p>Forecast: {forecastData.list.dt_txt}</p>}
+        {forecastData.list && <p>Temperature: {forecastData.list.main.temp}</p>}
+        {forecastData.list && <p>Icon: {forecastData.list.weather.icon}</p>}
+        {forecastData.list && <p>Description: {forecastData.list.weather.description}</p>}
+      </div> */}
     </div>
   );
 }
