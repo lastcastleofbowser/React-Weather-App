f8452141e7a2defec21fbfb8884879f9250cf00d
lastcastleofbowser <123087687+lastcastleofbowser@users.noreply.github.com>
2023-07-22 12:07:27 +0100

    use staes set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { useErrorBoundary } from "react-error-boundary";
+
 function App() {
+  
+     
+      const API_KEY = process.env.API_KEY;
+  
+      const [location, setLocation] = useState(""); //should include country code ie {city name},{country code}
+      const [description, setDescription] = useState("");
+      const [temperature, setTemperature] = useState(0);
+      const [humidity, setHumidity] = useState(0);
+      const [windSpeed, setWindSpeed] = useState(0);
+      const [windDirection, setWindDirection] = useState(0);
+      const [pressure, setPressure] = useState(0);
+
+      const [weather, setWeather] = useState({});
+      const [forecast, setForecast] = useState([]);
+  
+      const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
+      const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=&${location}&appid=${API_KEY}`;
+      
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
